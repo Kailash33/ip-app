6ac1485f62c7d808c843fa3d76c4ad00e42a8ea5
Kailash Behera <kailash@cybrain.co.in>
2022-09-06 13:11:30 +0530

    ip update

--- api/ip.js
+++ api/ip.js
@@ -1,18 +1,38 @@
 const Express = require('express');
-// const fetch = require('node-fetch');
 
+const DeviceDetector = require('node-device-detector');
+const detector = new DeviceDetector({
+    clientIndexes: true,
+    deviceIndexes: true,
+    deviceAliasCode: true,
+
+});
 
 const IpRoutes = Express.Router();
 
+IpRoutes.get('/client-info', (req, res) => {
+    return res.status(200).jsonp({
+        status: true,
+        message: 'OK',
+        data: detector.detect(req.headers['user-agent'])
+    })
+})
+
 IpRoutes.get('/ip', (req, res, next) => {
+
+    const clientip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
     return res.status(200).json({
         status: true,
         message: 'OK',
         data: {
-            ip_address: req.ip,
+            ip_address: clientip,
             public_ip: req.socket.remoteAddress,
-            date: new Date().toString()
+            date: new Date().toString(),
+            client_ip: {
+                soscketIp: req.socket.remoteAddress,
+                forwardedIp: req.headers['x-forwarded-for']
+            }
         }
     })
 });
